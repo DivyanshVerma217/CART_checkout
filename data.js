26dfda436cad6edccf12a5e1296a87c8a8d04a2c
Divyansh Verma <69747865+DivyanshVerma217@users.noreply.github.com>
2021-06-03 18:42:23 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,32 @@
+const data = {
+    products: [
+        {
+            id: '1',
+            name: "Macbook",
+            price: 5000,
+            image:'http://picsum.photos/id/180/2400/1600'
+        },
+        {
+            id: '2',
+            name: "Steering wheel",
+            price: 6000,
+            image:"http://picsum.photos/id/1070/5472/3648"
+        },
+        {
+            id: '3',
+            name: "Bible",
+            price: 500,
+            image:"http://picsum.photos/id/1010/5184/3456"
+        },
+        {
+            id: '4',
+            name: "Vintage car wallpaper",
+            price: 500,
+            image:"http://picsum.photos/id/133/2742/1828"
+        },
+       
+        
+    ]
+};
+
+export default data;
